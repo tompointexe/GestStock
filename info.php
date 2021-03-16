1e411283787cb9aa52a4a3737636385342b624e6
Tom <tom.guillou10@gmail.com>
2021-03-16 11:51:22 +0100

    Update info.php

--- info.php
+++ info.php
@@ -42,7 +42,12 @@ if(isset($_GET['id']) AND $_GET['id'] > 0){
         </ul>
         <ul class="navbar-nav ml-auto">
           <li class="nav-item dropdown"> <a class="nav-link dropdown-toggle" href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <?php echo $_SESSION['username'];?> </a>
-            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink"> <a class="dropdown-item" href="#">Action</a> <a class="dropdown-item" href="#">Another action</a> <a class="dropdown-item" href="#">Something else here</a> </div>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink"> 
+            <!--  TODO : Menu -->
+              <a class="dropdown-item" href="#">Action</a> 
+              <a class="dropdown-item" href="#">Another action</a> 
+              <a class="dropdown-item" href="#">Something else here</a> 
+            </div>
           </li>
         </ul>
       </div>
